86165d1a3ff4737ce60382013fb3233c74f895ae
Aditi Ashok <aditi.ashok@tufts.edu>
2015-02-28 10:09:04 -0500

    first attempt at modifying js

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,15 @@
-// Replace this with your code...
+function init () {
+var img = new Image();
+img.addEventListener("load", function()) {
+//draw image
+    var c = document.getElementById("game_canvas");
+    var ctx = c.getContext("2d");
+    ctx.drawImage(img,1200,1200);
+
+}, false);
+img.src = "pacman10-hp-sprite.png";
+
+
+
+
+
