fabce64d5acd4eea6e4221729e5838d018227dfd
Aditi Ashok <aditi.ashok@tufts.edu>
2015-02-28 11:32:23 -0500

    moved ms. pacman; fixed dimensions

--- mspacman/game.js
+++ mspacman/game.js
@@ -6,11 +6,11 @@ my_img.src = "pacman10-hp-sprite.png";
 my_img.onload = function() {
   canvas = document.getElementById('game_canvas');
   ctx = canvas.getContext('2d');
-  //Draw Ms. Pacman
+  //Draw Blue Board
   ctx.drawImage(my_img, 320, 0, 464, 137, 0, 0, 464, 137);
 
-  //Draw Blue Board
-  ctx.drawImage(my_img, 81, 23, 19, 20, 0, 0, 19, 20);
+  //Draw Ms. Pacman
+  ctx.drawImage(my_img, 81, 23, 19, 20, 25, 63, 19, 20);
 
 };
 
