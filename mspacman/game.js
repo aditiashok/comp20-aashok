fe5b2dcb0edb7b8d1486fb02f9543db18e7f7e74
Aditi Ashok <aditi.ashok@tufts.edu>
2015-02-28 11:29:26 -0500

    drew blue board

--- mspacman/game.js
+++ mspacman/game.js
@@ -6,9 +6,12 @@ my_img.src = "pacman10-hp-sprite.png";
 my_img.onload = function() {
   canvas = document.getElementById('game_canvas');
   ctx = canvas.getContext('2d');
+  //Draw Ms. Pacman
+  ctx.drawImage(my_img, 320, 0, 464, 137, 0, 0, 464, 137);
 
-  //Ms. Pac Man Drawing
-  ctx.drawImage(my_img, 81, 23, 19, 20, 0, 80, 19, 20);
+  //Draw Blue Board
+  ctx.drawImage(my_img, 81, 23, 19, 20, 0, 0, 19, 20);
+
 };
 
 }
