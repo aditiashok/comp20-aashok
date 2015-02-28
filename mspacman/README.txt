d6df991391371430f9bf6e244d3c87aa5bb8c71b
Aditi Ashok <aditi.ashok@tufts.edu>
2015-02-28 11:35:04 -0500

    added README

---
+++ mspacman/README.txt
@@ -0,0 +1,10 @@
+README
+Aditi Ashok
+
+Correctly/Incorrectly Implemented Aspects: To the best of my knowledge, all
+aspects of this lab have been implemented correctly.
+
+Collaborators: Help from Ming and Piazza
+
+Hours Spent: 1
+
--- mspacman/game.js
+++ mspacman/game.js
@@ -6,6 +6,7 @@ my_img.src = "pacman10-hp-sprite.png";
 my_img.onload = function() {
   canvas = document.getElementById('game_canvas');
   ctx = canvas.getContext('2d');
+  
   //Draw Blue Board
   ctx.drawImage(my_img, 320, 0, 464, 137, 0, 0, 464, 137);
 
